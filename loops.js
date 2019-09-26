0049f62099187208b2f0a603024d7c33bdc054f7
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-26 03:45:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
     if (i===1 )
-      array[i]=console.log('I am 1 strange loop')
+      array[i]=console.log('I am 1 strange loop.')
     else
-      array[i]=console.log(`I am "${i}" strange loops.`)
+      array[i]=console.log(`I am ${i} strange loops.`)
   }
   return array
 }
