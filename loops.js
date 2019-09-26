33c859a2e788b185d0b8e9655579ccc642147eeb
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-26 04:19:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,12 +16,12 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(num) {
-  function incrementVariable(num) {
+  function incrementVariable() {
     num = num + 1
     return num
 }
   do {
     console.log("I run once regardless.")
-    incrementVariable(num)
-  } while (incrementVariable(num)<num)
+    incrementVariable()
+  } while (incrementVariable()<num)
 }
